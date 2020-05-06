87fd46fa78ce6c377872491fcf09d4a6172b2591
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-05-06 14:30:52 +0530

    New Form

---
+++ OPEN/register.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+$server = "localhost";
+$user = "root";
+$password = "";
+$dbname = "open";
+$mysqli = new mysqli($server, $user, $password, $dbname);
+ 
+//Checking connection
+ 
+if($mysqli->connect_error){
+ die("Connection failed:" . $mysqli->connect_error);
+}
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $pass1= $_POST['pass1'];
+
+
+    // Password Check
+    if($pass1 != $pass){
+        echo"Password do not match!";
+    }
+    else{
+    $passfinal = md5($pass);
+    $q="INSERT INTO user (email,pass) values ('$email','$passfinal')";
+    
+    if ($mysqli->query($q) === TRUE){
+        echo "SUCCESS";
+    }
+    else {
+        echo "Error in Saving!";
+    }
+    $mysqli->close();
+}
+
+?>
